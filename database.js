3077937371be23ab5069128ff62255f3cb0eb6ba
michaelchum <michaelchumho@gmail.com>
2013-10-06 05:23:33 -0400

    Added core firebase and structured database

--- database.js
+++ database.js
@@ -1,18 +1,28 @@
 var dealSMS = require('./sms.js');
 var dealEMAIL = require('./email.js');
+var dealCRAWLER = require('./crawler.js');
+
+var USER_LIST_LOCATION = 'https://cheapcheep.firebaseio.com/user_list';
+var EMAIL_INDEX = 'https://cheapcheep.firebaseio.com/email_index';
+var PHONE_INDEX = 'https://cheapcheep.firebaseio.com/phone_index';
+var KEYWORDS_INDEX = 'https://cheapcheep.firebaseio.com/keywords_index';
 
 // Initialize firebase
 var Firebase = require('firebase');
+var userList = new Firebase(USER_LIST_LOCATION);
+var emailList = new Firebase(EMAIL_INDEX);
+var phoneList = new Firebase(PHONE_INDEX);
+var keywordsList = new Firebase(KEYWORDS_INDEX);
 
-var listRef = new Firebase('https://cheapcheep.firebaseio.com/user_list');
-listRef.on('mickeyho92@gmail.com', function(snapshot) {
-  if(snapshot.val() === null) {
-    alert('User julie does not exist.');
-  } else {
-    var email = snapshot.val().email;
-    var email = snapshot.val().email;
-    alert('The e-mail is: ' + email + ' and phone is ' + phone + ' keywords are ' + array);
-  }
+userList.once('child_added', function(snapshot) {
+  var userID = snapshot.name(), userData = snapshot.val();
+  emailList.child(userData.email).set({userID: userID});
+  phoneList.child(userData.telephone).set({userID: userID});
 });
 
-
+userList.on('child_changed', function(snapshot) {
+  var userID = snapshot.name(), userData = snapshot.val();
+  for (var i = 0; i < userData.keywords.length; i++){
+  	keywordsList.child(userData.keywords[i]).set({userID: userID});
+  }
+});
